00f29da2ae9fc4b10fa95b8df9a3b77d9f71f190
mayanem <mayanemesquitta@gmail.com>
2023-07-04 23:19:47 -0400

    Initial commit

---
+++ aditamento/src/test/java/br/com/itau/aditamento/AditamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.aditamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AditamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
